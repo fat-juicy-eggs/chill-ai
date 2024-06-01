10facfc31269044e58dbe6bda12f4b3e19309451
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-01 17:30:47 -0400

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,17 +15,19 @@ const Hero = () => {
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
-              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
+              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero">
                 CHILL AI Project Website
               </h1>
-              <p>
+              <p className="text-black font-bold text-2x1">
                 Computational Heat Island Localization & Limitation (or a group of high school students committed to a cause)
               </p>
-              <a href="#"
-                <button className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho">
-                  Vote for us!
-                </button>
-              </a>
+              <div className="mt-10">
+                <a href="#"
+                  <button aria-label="get started button" className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho">
+                    Vote for us!
+                  </button>
+                </a>
+              </div>
             </div>
             <div className="animate_right hidden md:w-1/2 lg:block">
               <div className="relative 2xl:-mr-7.5">
