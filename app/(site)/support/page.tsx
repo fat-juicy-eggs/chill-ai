06d6020e1a5c83c1d376607c0c41b7f120ad4127
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-01 15:55:18 -0400

    Update page.tsx

--- app/(site)/support/page.tsx
+++ app/(site)/support/page.tsx
@@ -3,8 +3,8 @@ import Contact from "@/components/Contact";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Support Page - Solid SaaS Boilerplate",
-  description: "This is Support page for Solid Pro",
+  title: "Support us!",
+  description: "Help us take first place in the Shape the Future Challenge",
   // other metadata
 };
 
