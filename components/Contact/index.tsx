46ee7e8aaa901efb6bc9b91df9227fc1dcc4fc79
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-01 19:07:54 -0400

    Update and rename index.tsx to index.tsx

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -5,11 +5,9 @@ import React from "react";
 
 const Contact = () => { return (
     <>
-      {/* <!-- ===== Contact Start ===== --> */}
       <section id="support" className="px-4 md:px-8 2xl:px-0">
         <p>Hi</p>
       </section>
-      {/* <!-- ===== Contact End ===== --> */}
     </>
   );
 };
