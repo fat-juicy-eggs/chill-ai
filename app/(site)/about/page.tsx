8b7b65a7b9d54b273d72fb8e89c413f189286088
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-02 19:05:17 -0400

    Update page.tsx

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import Intro from "@/components/Intro";
+import Blog from "@/components/Blog";
 import React from "react";
 
 export const metadata: Metadata = {
@@ -12,6 +13,7 @@ export default function Home() {
   return (
     <>
       <Intro />
+      <Blog />
     </>
   );
 }
