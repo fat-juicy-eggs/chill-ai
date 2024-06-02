7e8c29da556a4d74b2b051c49fa68bdba4176ae9
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-02 13:36:36 -0400

    Update page.tsx

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -9,8 +9,17 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
-    </main>
+    <section className="pb-16 pt-24 md:pb-20 md:pt-28 lg:pb-24 lg:pt-32">
+      <div className="container mx-auto">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="blog-details blog-details-docs shadow-three dark:bg-gray-dark rounded-sm bg-white px-8 py-11 sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]">
+            <h1 id="0">Our product</h1>
+            <p>
+              Cooler cities, brighter futures.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
