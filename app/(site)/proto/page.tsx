087f36af44158a9c1e5602f36cb6d0e49c2c1fc6
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-02 11:56:06 -0400

    Update page.tsx

--- app/(site)/proto/page.tsx
+++ app/(site)/proto/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import Feature from "@/components/Features";
+import About from "@/components/About";
 
 export const metadata: Metadata = {
   title: "Prototype",
@@ -10,7 +10,7 @@ export default function Prototype() {
   return (
     <>
       <section>
-        <Feature />
+        <About />
       </section>
     </>
   );
