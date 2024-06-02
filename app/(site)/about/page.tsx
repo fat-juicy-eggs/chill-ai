fe42948d59eaf96b8beaf63cdb9269a23a4cb192
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-02 14:15:05 -0400

    Update page.tsx

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -12,13 +12,12 @@ export default function Home() {
     <>
       <section id="support" className="px-4 md:px-8 2xl:px-0">
         <div className="relative mx-auto max-w-c-1390 px-7.5 pt-10 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20">
-          <div className="absolute left-0 top-0 -z-1 h-2/3 w-full rounded-lg bg-gradient-to-t from-transparent to-[#dee7ff47] dark:bg-gradient-to-t dark:to-[#252A42]"></div>
-            <div className="absolute bottom-[-255px] left-0 -z-1 h-full w-full">
-              <h1 id="0">Our product</h1>
-              <p>
-                Cooler cities, brighter futures.
-              </p>
-            </div>
+          <div className="absolute left-0 top-0 -z-1 h-2/3 w-full rounded-lg bg-gradient-to-t from-transparent to-[#dee7ff47]"></div>
+          <div className="absolute bottom-[-255px] left-0 -z-1 h-full w-full">
+            <h1 id="0">Our product</h1>
+            <p>
+              Cooler cities, brighter futures.
+            </p>
           </div>
         </div>
       </section>
