604010e4a536fef0da13b5a796c53640585a9011
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-02 19:11:19 -0400

    Update page.tsx

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import Intro from "@/components/Intro";
 import Blog from "@/components/Blog";
+import BlogData from "@/components/Blog-board/blogData";
+import BlogItem from "@/components/Blog-board/BlogItem";
 import React from "react";
 
 export const metadata: Metadata = {
@@ -14,7 +16,7 @@ export default function Home() {
     <>
       <Intro />
       <section className="py-20 lg:py-25 xl:py-30">
-        <div className="mx-auto mt-15 max-w-c-1280 px-4 md:px-8 xl:mt-20 xl:px-0">
+        <div className="mx-auto mt-5 max-w-c-1280 px-4 md:px-8 xl:mt-20 xl:px-0">
           <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
             {BlogData.map((post, key) => (
               <BlogItem key={key} blog={post} />
