a6833886a879d615c57037cb5b964279b32a1dec
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-02 18:21:14 -0400

    Update index.tsx

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -1,42 +1,70 @@
 "use client";
 import { motion } from "framer-motion";
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 const Intro = () => {
   return (
     <>
-      <section id="support" className="px-4 md:px-8 2xl:px-0">
-        <div className="relative mx-auto max-w-c-1390 px-7.5 pt-10 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20">
-          <div className="absolute left-0 top-0 -z-1 h-2/3 w-full rounded-lg bg-gradient-to-t from-transparent to-[#dee7ff47]"></div>
-          <div className="absolute bottom-[-255px] left-0 -z-1 h-full w-full">
-            <div className="flex flex-col-reverse flex-wrap gap-8 md:flex-row md:flex-nowrap md:justify-between xl:gap-20">
-              <motion.div
-                variants={{
-                  hidden: {
-                    opacity: 0,
-                    y: -20,
-                  },
-                  visible: {
-                    opacity: 1,
-                    y: 0,
-                  },
-                }}
-                initial="hidden"
-                whileInView="visible"
-                transition={{ duration: 1, delay: 0.1 }}
-                viewport={{ once: true }}
-                className="animate_top w-full rounded-lg bg-white p-7.5 shadow-solid-8 dark:border dark:border-strokedark dark:bg-black md:w-3/5 lg:w-3/4 xl:p-15"
-              >
-                <h2 className="mb-15 text-3xl font-semibold text-black dark:text-white xl:text-sectiontitle2">
-                  Send a message
-                </h2>
-              </motion.div>
-            </div>
+      <section className="pb-12.5 pt-32.5 lg:pb-25 lg:pt-45 xl:pb-30 xl:pt-50">
+        <div className="relative z-1 mx-auto max-w-c-1016 px-7.5 pb-7.5 pt-10 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20">
+          <div className="absolute left-0 top-0 -z-1 h-2/3 w-full rounded-lg bg-gradient-to-t from-transparent to-[#dee7ff47] dark:bg-gradient-to-t dark:to-[#252A42]"></div>
+          <div className="absolute bottom-17.5 left-0 -z-1 h-1/3 w-full">
+            <Image
+              src="/images/shape/shape-dotted-light.svg"
+              alt="Dotted"
+              className="dark:hidden"
+              fill
+            />
+            <Image
+              src="/images/shape/shape-dotted-dark.svg"
+              alt="Dotted"
+              className="hidden dark:block"
+              fill
+            />
           </div>
+
+          <motion.div
+            variants={{
+              hidden: {
+                opacity: 0,
+                y: -20,
+              },
+
+              visible: {
+                opacity: 1,
+                y: 0,
+              },
+            }}
+            initial="hidden"
+            whileInView="visible"
+            transition={{ duration: 1, delay: 0.1 }}
+            viewport={{ once: true }}
+            className="animate_top rounded-lg bg-white px-7.5 pt-7.5 shadow-solid-8 dark:border dark:border-strokedark dark:bg-black xl:px-15 xl:pt-15"
+          >
+            <h2 className="mb-5 text-center text-3xl font-semibold text-black dark:text-white xl:text-sectiontitle2">
+              Urban Heat Islands
+            </h2>
+            <p className="mb-2 text-dark font-bold">
+              What are they?
+            </p>
+            <ul className="list-disc ml-10 font-medium text-dark mb-5">
+              <li>
+                Metropolitan areas warmer than the surrounding environment
+              </li>
+              <li>
+                Caused by natural land being replaced with artificial heat-retaining materials
+              </li>
+              <li>
+                Drastically increases health risks from high temperatures and air pollution
+              </li>
+            </ul>
+          </motion.div>
         </div>
       </section>
     </>
   );
 };
 
-export default Intro;
+export default Signup;
