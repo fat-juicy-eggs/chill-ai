669ecd8f599cd9a8a3893fb1cc43837324aa21b7
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-02 20:57:44 -0400

    Update index.tsx

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -5,9 +5,11 @@ import React from "react";
 
 const Contact = () => { return (
     <>
-      <section id="support" className="px-4 md:px-8 2xl:px-0">
-        <p>Hi</p>
-      </section>
+      <section className="pb-12.5 pt-32.5 lg:pb-25 lg:pt-45 xl:pb-30 xl:pt-50">
+        <div className="relative z-1 mx-auto max-w-c-1016 px-7.5 pb-7.5 pt-10 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20">
+            
+        </div>
+      </section>  
     </>
   );
 };
