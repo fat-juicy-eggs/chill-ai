e3987ee2d5b1b8a44af0a16413a0ac57442e101b
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-02 21:08:06 -0400

    Update page.tsx

--- app/(site)/meet-the-team/page.tsx
+++ app/(site)/meet-the-team/page.tsx
@@ -14,15 +14,20 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <Intro />
-      <div className="mx-auto mt-5 max-w-c-1280 px-4 md:px-8 xl:mt-20 xl:px-0">
-        <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
-          {BlogData.map((post, key) => (
-            <BlogItem key={key} blog={post} />
-          ))}
+      <section className="pb-12.5 pt-32.5 lg:pb-25 lg:pt-45 xl:pb-30 xl:pt-50">
+        <div className="relative z-1 mx-auto max-w-c-1016 px-7.5 pb-7.5 pt-10 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20">
+          <h2 className="mb-5 text-center text-3xl font-semibold text-black dark:text-white xl:text-sectiontitle2">
+            Our Team
+          </h2>
+          <div className="mx-auto mt-5 max-w-c-1280 px-4 md:px-8 xl:mt-20 xl:px-0">
+            <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
+              {BlogData.map((post, key) => (
+                <BlogItem key={key} blog={post} />
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-
+      </section>
     </>
   );
 }
