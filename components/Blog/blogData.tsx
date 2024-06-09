59255f7b8317cb3797a02140a3827e31071af1d6
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-09 11:15:04 -0400

    Update blogData.tsx

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -31,17 +31,17 @@ const BlogData: Blog[] = [
   },
   {
     _id: 2,
-    mainImage: "/images/blog/blog-04.png",
+    mainImage: "/images/blog/danielyi.jpg",
     title: "Daniel Yi",
     metadata:
-      "Fumbling along",
+      "Hey, I'm Daniel and I'm a rising senior from Rockville, Maryland. In my free time, I enjoy scrolling Wikipedia and learning about anything that intrigues me. As of now, I'm learning photolithography and ML/AI applications, and I hope to become an ML or computer engineer. ",
   },
   {
     _id: 2,
-    mainImage: "/images/blog/blog-01.png",
+    mainImage: "/images/blog/jasonlu.jpg",
     title: "Jason Lu",
     metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
+      "Hi, I’m Jason and I’m a rising senior from Baltimore, Maryland. I love taking walks, learning new things about the environment, and working with computers. Currently, I’m involved in the NASA SEES and MITES Semester programs. In the future I hope to become either a computer or environmental engineer.",
   },
 ];
 
