d74a05d7db503cfb3c476467908c88c9f804e63d
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-09 20:40:30 -0400

    Update blogData.tsx

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -3,21 +3,21 @@ import { Blog } from "@/types/blog";
 const BlogData: Blog[] = [
   {
     _id: 1,
-    mainImage: "/images/blog/blog-01.png",
+    mainImage: "/images/blog/brendonwu.jpg",
     title: "Brendon Wu",
     metadata:
-      "The man, the myth, the legend.",
+      "Hi, I'm Brendon and I'm a rising junior from Los Angeles. My passions lie in the intersections between biomedicine and AI, with a focus on Computer Vision applications in medical imaging. I'd like to thank Moonshot Pirates for providing us this amazing opportunity to dive deeper into environmental sustainability and entrepreneurship!",
   },
   {
     _id: 2,
-    mainImage: "/images/blog/blog-02.png",
+    mainImage: "/images/blog/justinlam.png",
     title: "Justin Lam",
     metadata:
-      '"I was just a business man, doing business"',
+      "Hi, I'm Justin and I'm a rising senior from Los Angeles, California. I am extremely passionate about business, mathematics, and physics. I’m part of my school’s Speech and Debate team, basketball team, FBLA team, and MESA club. In my free time, I love doing Khan Academy or sleeping.",
   },
   {
     _id: 2,
-    mainImage: "/images/blog/blog-03.png",
+    mainImage: "/images/blog/czhou.png",
     title: "Charlotte Zhou",
     metadata:
       "Video person",
@@ -27,7 +27,7 @@ const BlogData: Blog[] = [
     mainImage: "/images/blog/danielyi.jpg",
     title: "Daniel Yi",
     metadata:
-      "Hey, I'm Daniel and I'm a rising senior from Rockville, Maryland. In my free time, I enjoy scrolling Wikipedia and learning about anything that intrigues me. As of now, I'm learning photolithography and ML/AI applications, and I hope to become an ML or computer engineer. ",
+      "Hey, I'm Daniel and I'm a rising senior from Rockville, Maryland. In my free time, I enjoy scrolling Wikipedia and learning about anything that intrigues me. As of now, I'm learning photolithography and ML/AI applications, and I hope to become an ML or computer engineer (perhaps I would also consider physics).",
   },
   {
     _id: 2,
