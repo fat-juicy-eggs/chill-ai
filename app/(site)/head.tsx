31347384c44133c62ea44d3585771b7fd3eea84d
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-07-03 18:38:09 -0400

    Update head.tsx

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -6,6 +6,8 @@ export default function Head() {
       <meta property="og:image" content="./chilllogo.png" />
       <meta property="og:title" content="CHILL AI" />
       <meta property="og:description" content="Computational Heat Island Localization and Limitation. Cooler cities, brighter futures." />
+      <meta property="og:image:width" content="1280" />
+      <meta property="og:image:height" content="720" />
       <link rel="icon" href="/app/favicon.ico" />
     </>
   );
