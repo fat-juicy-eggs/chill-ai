351593cc35257c768a7fbdbdfb06f141a0af6f12
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-07-03 18:25:35 -0400

    Update head.tsx

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -3,7 +3,9 @@ export default function Head() {
     <>
       <title> CHILL AI</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Built with Next.js and TypeScript" />
+      <meta property="og:image" content="./chilllogo.png" />
+      <meta property="og:title" content="CHILL AI" />
+      <meta property="og:description" content="Computational Heat Island Localization and Limitation. Cooler cities, brighter futures." />
       <link rel="icon" href="/app/favicon.ico" />
     </>
   );
